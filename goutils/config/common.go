7e11144d8f89ca9cff745a2c235f9201a77da491
bvv <vblvbl133@gmail.com>
2026-08-15 14:27:14 +0300

    add utils

--- goutils/config/common.go
+++ goutils/config/common.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"slices"
+	"strconv"
+
+	"github.com/bvv_utils/goutils/mistake"
+)
+
 type StandardConfig struct {
 	App struct {
 		// PromoHost string `conf:"env:PROMO_HOST"`
@@ -13,6 +20,25 @@ type StandardConfig struct {
 	- goto https://pkg.go.dev/go.uber.org/zap#pkg-constants
 	*/
 	LogLevel string `conf:"default:INFO,env:LOGGER_LEVEL"`
+}
+
+func (s *StandardConfig) GetApiPort() string {
+	return s.App.ApiPort
+}
+func (s *StandardConfig) GetLogLevel() string {
+	return s.LogLevel
+}
+
+func (c *StandardConfig) ValidateStandardConfig() error {
+	if i, err := strconv.Atoi(c.GetApiPort()); err != nil {
+		return err
+	} else if i < 1001 || i > 64000 {
+		return mistake.ErrIncorrectApiPortValue
+	}
+	if !slices.Contains([]string{"DEBUG", "WARN", "ERROR", "PANIC", "DPANIC", "FATAL"}, c.GetLogLevel()) {
+		return mistake.ErrIncorrectLogLevel
+	}
+	return nil
 }
 
 // use this structure for parallel database access patterns:
